56e4c4a228d2a4fee829c4a49378ccb18c9ff6af
fkam <felix.kam@live.se>
2018-03-12 14:57:09 +0100

    feat #8, added testcase

---
+++ Decide/src/test/java/DD2480/decide/CMVTest.java
@@ -0,0 +1,36 @@
+package DD2480.decide;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import java.util.Arrays;
+
+public class CMVTest{
+    
+
+    @Test
+    public void CMVlicZero(){
+    
+        boolean[] CMV = new boolean[15];
+        Parameter parameter = new Parameter();
+            
+        Points[] dataPoints = {
+            new Points(0.0, 0.0),
+            new Points(5.0, 5.0),
+            new Points(4.0, 5.0),
+            new Points(7.0, 9.0)
+        };
+
+        CMVHandler handler = new CMVHandler(parameter, dataPoints);
+        parameter.length1 = 7;
+        CMV = handler.generateCMV();
+        assertTrue(CMV[0]);
+        
+        parameter.length1 = 10;
+        CMV = handler.generateCMV();
+        assertFalse(CMV[0]);
+
+    }
+
+
+}
