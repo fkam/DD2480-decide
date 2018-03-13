7f57bddb9cbed4fb2e05cc70fa1e09a7d944dd0d
KanskeMarcus <mlig@kth.se>
2018-03-13 16:13:05 +0100

    (FIX:#28) changed the function from using polar coordinates
    Also fixed the tests to be more in line of how to properly compare floating point values

--- Decide/src/main/java/DD2480/decide/CMVHandler.java
+++ Decide/src/main/java/DD2480/decide/CMVHandler.java
@@ -35,12 +35,65 @@ public class CMVHandler {
 	}
 
 	private boolean licOne() {
-		// TODO Auto-generated method stub
+		if(dataPoints.length<3){
+			return false;
+		}
+		//first 3 consecutive datapoints
+		Points first = dataPoints[0];
+		Points second = dataPoints[1];
+		Points third = dataPoints[2];
+		boolean keepSearching = (dataPoints.length>3) ? true : false;
+		for(int i = 2; i<dataPoints.length;){
+			//calculate point equidistant from all 3 points
+			double centerX = (first.x + second.x + third.x)/3;
+			double centerY = (first.y + second.y + third.y)/3;
+			Points center = new Points(centerX,centerY);
+			//if the distance is between the center of the circle and the 
+			//3 points return true
+			if(center.distance(first) > parameters.radius1 ||
+			   center.distance(second)> parameters.radius1 ||
+			   center.distance(third) > parameters.radius1){
+				return true;
+			}
+			i++;
+			//shift the consecutive datapoint forward one step if we have a set of points larger than 3
+			if(keepSearching){
+				first = second;
+				second = third;
+				third = dataPoints[i];
+			}
+		}
+		//if no point found return false
 		return false;
 	}
 
 	private boolean licTwo() {
-		// TODO Auto-generated method stub
+		if(dataPoints.length<3){
+			return false;
+		}
+		boolean keepSearching = (dataPoints.length>3) ? true : false;
+		//first 3 consecutive datapoints
+		Points first = dataPoints[0];
+		Points second = dataPoints[1];
+		Points third = dataPoints[2];
+		for(int i = 2;i <dataPoints.length;){
+			//considered unsatisifed if any of point 1 or 3 is coincide with point 2
+			if((first.x==second.x&&first.y==second.y)||(third.x==second.x)&&third.y==second.y){
+				continue;
+			}
+			//get angle see if it is within the boundries
+			double angle = GeometryHelper.angle(first,second,third);
+			if((angle<Math.PI -parameters.epsilon)||(angle>Math.PI+parameters.epsilon)){
+				return true;
+			}
+
+			i++;
+			if(keepSearching){
+				first = second;
+				second = third;
+				third = dataPoints[i];
+			}
+		}
 		return false;
 	}
 
