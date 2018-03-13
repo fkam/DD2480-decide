5fdd89b38aea75b877a1ca4c7453590bcc37df81
pwnttm <marcus.lignercrona@gmail.com>
2018-03-13 12:00:18 +0100

    (FEAT:#28) added angle function to get angle of 3 points with tests (#29)

--- Decide/src/main/java/DD2480/decide/GeometryHelper.java
+++ Decide/src/main/java/DD2480/decide/GeometryHelper.java
@@ -1,5 +1,15 @@
 package DD2480.decide;
-
+import java.lang.Math;
 public class GeometryHelper {
-
+	/**
+	 *@param 3 Points in space where b does not coincide with a or c
+	 *@return the angle betweeen lines ba and bc
+	 */
+	public static double angle(Points a, Points b, Points c){
+		//Convert polar coordinate ba / bc become angle theta
+		double ba = Math.atan2((a.x - b.x),(a.y - b.y));
+		double bc = Math.atan2((c.x - b.x),(c.y -b.y));
+		// return difference of angles
+		return ba - bc;
+	}
 }
