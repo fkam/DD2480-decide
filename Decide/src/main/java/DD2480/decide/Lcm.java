0a3c07bda448953bce48df8da6b22f713a10c423
pwnttm <marcus.lignercrona@gmail.com>
2018-03-15 10:17:23 +0100

    (FEAT: #42) Lcm
    * (TYPO: added identifier and correct class calls to area)
    
    * (FEAT:#35) added a constructor with getter and setter to Lcm

--- Decide/src/main/java/DD2480/decide/Lcm.java
+++ Decide/src/main/java/DD2480/decide/Lcm.java
@@ -1,7 +1,26 @@
 package DD2480.decide;
 
-public enum Lcm {
-    ANDD,
-    ORR,
-    NOTUSED;
+public class Lcm{
+	public enum lcmElement {
+	    ANDD,
+	    ORR,
+	    NOTUSED;
+	}
+	lcmElement[][] theMatrix = new lcmElement[15][15];
+	/**
+	 * initialise matrix of lcm elements to contain only ANDD 
+	 */
+	public Lcm(){
+		for(int i = 0;i<15;i++){
+			for(int j = 0; j<15;j++){
+				theMatrix[i][j] = lcmElement.ANDD;
+			}
+		}
+	}
+	public lcmElement getElement(int i,int j){
+		return theMatrix[i][j];
+	}
+	public void setElement(int i,int j,lcmElement value){
+		theMatrix[i][j] = value;
+	}
 }
